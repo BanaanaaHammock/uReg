ba53ab1b539ccb465df76b3d16950093d8868fc2
Jon Malave <jonmalave@gmail.com>
2016-11-24 07:54:45 -0500

    Update uReg.php

--- uReg.php
+++ uReg.php
@@ -7,17 +7,15 @@ function uReg() {
 	
 	// register new user if set to true
 	$addUser = true;
-
+	
 	// open database, search for existing user and authenticate, or register new user
 	if ($db = opendir("Database")) {
 		while (false !== ($userData = readdir($db))) {
 			if ($userData === "$id.php") {
 				// user exists; disable add user event
 				$addUser = false;
-				
 				// retrieve password from database
 				include_once("Database/$userData");	
-				
 				// authenticate with password
 				if ($pw === $userPass){
 					// return successful authentication
@@ -27,30 +25,22 @@ function uReg() {
 					// return un-successful authentication					
 					echo "invalid password";
 				}
-			
 			}			
 		}
-		
 		// register & authenticate new user
 		if($addUser && $id !== "" && $pw !== ""){
 			// specify user id
 			$newUser = "Database/$id.php";
-			
 			// create user account in database
 			fopen($newUser, "w");	
-			
 			// access the new user account				
 			$addData = file_get_contents($newUser);
-			
 			// set password to append to user account
 			$addData .= '<?php $userPass = ' . "'" . $pw . "'" . '; ?>';
-			
 			// save password data to user account
 			file_put_contents($newUser, $addData);
-			
 			// return successful user registration and authentication
 			echo "ok";
-			
 			// uncomment below for testing user registration
 			//echo "new user created!";
 		}
